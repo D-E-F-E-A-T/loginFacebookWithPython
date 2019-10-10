5dad4102a898a7bb3e1c755869fb1dd1eef2d7d4
Vinicius  Pereira <viniciuspoa2@hotmail.com>
2019-10-10 12:42:13 -0500

    Update theFacebook-login.py

--- theFacebook-login.py
+++ theFacebook-login.py
@@ -3,9 +3,9 @@ import mechanize
 browser = mechanize.Browser()
 browser.set_handle_robots(False)
 browser.addheaders = [('User-agent','Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36')]
-browser.open('https://www.linkedin.com/')
+browser.open('https://www.facebook.com/login.php)
 browser.select_form(nr=0)
-browser.form['username']="viniciuspoa2@hotmail.com"
-browser.form['password']="sesbdv7t10"
+browser.form['username']=""
+browser.form['password']=""
 submit =  browser.submit()
 print(submit.geturl())
